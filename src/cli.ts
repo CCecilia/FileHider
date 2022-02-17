3b209e85c90e364821252182cee39940cf9d1f69
Christian Cecilia <christian.cecilia1@gmail.com>
2022-02-16 23:00:48 -0500

    add formatter, yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
+
+yargs(hideBin(process.argv))
+  .commandDir('commands')
+  .strict()
+  .alias({ h: 'help' }).argv;
